8edaecc9ebcfeff958bc87ce9950083c75cd6490
Ramon Conrado <ramonconrado94@gmail.com>
2020-07-07 18:06:30 -0300

    estrutura inicial

---
+++ app/Controllers/Http/AppController.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class AppController {
+    index(){
+        return "Relaxô serviços";
+    }
+}
+
+module.exports = AppController
---
+++ database/migrations/1594155813565_user_image_fk_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserImageFkSchema extends Schema {
+  up() {
+    this.table('users', (table) => {
+      // alter table
+      table.foreing('image_id').references('id').inTable('images').onDelete('cascade')
+    })
+  }
+
+  down() {
+    this.table('users', (table) => {
+      // reverse alternations
+      table.dropForeing('image_id') 
+    })
+  }
+}
+
+module.exports = UserImageFkSchema
